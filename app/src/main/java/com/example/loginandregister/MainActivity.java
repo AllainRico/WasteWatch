2bb832a08c8e466f56e238cf07ba4a86b366f31d
Earl <earlpaquibut@gmail.com>
2023-07-27 01:39:07 +0800

    Added Fragments and added TextView to manage Profile

--- app/src/main/java/com/example/loginandregister/MainActivity.java
+++ app/src/main/java/com/example/loginandregister/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.loginandregister;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,31 +12,49 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
+import com.example.loginandregister.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    //Inputs
-    Button logout;
-    TextView user;
-
+    //Fragment
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        //Inputs
-        logout = findViewById(R.id.logout);
+        //Fragment shown first at start
+        replaceFragment(new HomeFragment());
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-                finish();
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.schedule) {
+                replaceFragment(new ScheduleFragment());
+            } else if (itemId == R.id.map) {
+                replaceFragment(new MapFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new ProfileFragment());
             }
+
+            return true;
         });
+
+    }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+
+        fragmentTransaction.commit();
+
     }
 }
---
+++ app/src/main/java/com/example/loginandregister/ProfileFragment.java
@@ -0,0 +1,43 @@
+package com.example.loginandregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class ProfileFragment extends Fragment {
+
+    Button buttonLogout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        buttonLogout = view.findViewById(R.id.btn_logout);
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Login.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        return view;
+    }
+}
