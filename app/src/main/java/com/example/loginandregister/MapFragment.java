db90fbe9776ad929947786dd8d6b60de488e06a8
Earl <earlpaquibut@gmail.com>
2023-07-29 21:49:10 +0800

    Updated map layout w/o API yet

--- app/src/main/java/com/example/loginandregister/MapFragment.java
+++ app/src/main/java/com/example/loginandregister/MapFragment.java
@@ -2,20 +2,35 @@ package com.example.loginandregister;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MapFragment extends Fragment {
-
+public class MapFragment extends Fragment implements OnMapReadyCallback{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        // Get the hosting FragmentActivity and then access the SupportMapFragment
+        FragmentActivity activity = requireActivity();
+        SupportMapFragment mapFragment = (SupportMapFragment) activity.getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
