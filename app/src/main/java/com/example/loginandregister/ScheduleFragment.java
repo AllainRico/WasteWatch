38603c09b4e2f58a6de70befd02519e9a1f8e4e1
Earl <earlpaquibut@gmail.com>
2023-07-30 21:55:56 +0800

    Remaked Schedule

--- app/src/main/java/com/example/loginandregister/ScheduleFragment.java
+++ app/src/main/java/com/example/loginandregister/ScheduleFragment.java
@@ -1,57 +1,104 @@
-package com.example.loginandregister;
+    package com.example.loginandregister;
 
-import android.os.Bundle;
+    import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+    import androidx.fragment.app.Fragment;
+    import androidx.recyclerview.widget.GridLayoutManager;
+    import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CalendarView;
-import android.widget.Toast;
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.view.ViewGroup;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
+    import java.time.LocalDate;
+    import java.time.YearMonth;
+    import java.time.format.DateTimeFormatter;
+    import java.util.ArrayList;
+    import java.util.Locale;
 
-public class ScheduleFragment extends Fragment {
+    public class ScheduleFragment extends Fragment implements CalendarAdapter.OnItemListener{
 
-    CalendarView calendarView;
-    Calendar calendar;
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        private TextView monthYearText;
+        private RecyclerView calendarRecyclerView;
+        private LocalDate selectedDate;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View view = inflater.inflate(R.layout.fragment_schedule, container, false);
 
-        calendarView = view.findViewById(R.id.calendar_view);
-        calendar = Calendar.getInstance();
+            initWidgets(view);
+            selectedDate = LocalDate.now();
+            setMonthView();
 
-        setDate(28,7,2023);
-        getDate();
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
-                Toast.makeText(getActivity(), day + "/" + month + "/" + year, Toast.LENGTH_SHORT).show();
+            return view;
+        }
+
+        private void initWidgets(View view) {
+            calendarRecyclerView = view.findViewById(R.id.caledarRecycleView);
+            monthYearText = view.findViewById(R.id.monthYearTV);
+        }
+
+        private void setMonthView() {
+            monthYearText.setText(monthYearFromDate((selectedDate)));
+            ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+            CalendarAdapter calendarAdapter = new CalendarAdapter(this, daysInMonth);
+            RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), 7);
+            calendarRecyclerView.setLayoutManager(layoutManager);
+            calendarRecyclerView.setAdapter(calendarAdapter);
+
+        }
+
+        private String monthYearFromDate(LocalDate date){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy", Locale.getDefault());
+            return date.format(formatter);
+        }
+
+        private ArrayList<String> daysInMonthArray(LocalDate date) {
+            ArrayList<String> daysInMonthArray = new ArrayList<>();
+            YearMonth yearMonth = YearMonth.from(date);
+            int daysInMonth = yearMonth.lengthOfMonth();
+            LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+
+            int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+            // Calculate the day number for the first day of the calendar grid
+            int startDay = 1 - dayOfWeek;
+
+            for (int i = startDay; i <= daysInMonth; i++) {
+                if (i <= 0 || i > daysInMonth) {
+                    daysInMonthArray.add("");
+                } else {
+                    daysInMonthArray.add(String.valueOf(i));
+                }
             }
-        });
 
-        return view;
-    }
-    public void getDate(){
-        long date = calendarView.getDate();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yy", Locale.getDefault());
-        calendar.setTimeInMillis(date);
-        String select_date = simpleDateFormat.format(calendar.getTime());
-        Toast.makeText(getActivity(), select_date, Toast.LENGTH_SHORT).show();
-    }
+            // Add extra empty cells at the end to fill the calendar grid
+            while (daysInMonthArray.size() < 42) {
+                daysInMonthArray.add("");
+            }
 
-    public void setDate(int day, int month, int year){
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month - 1);
-        calendar.set(Calendar.DAY_OF_MONTH, day);
-        long milli = calendar.getTimeInMillis();
-        calendarView.setDate(milli);
+            return daysInMonthArray;
+        }
+
+
+        public void previousMonth(View view) {
+            selectedDate = selectedDate.minusMonths(1);
+            setMonthView();
+        }
+
+        public void nextMonth(View view) {
+            selectedDate = selectedDate.plusMonths(1);
+            setMonthView();
+        }
+
+        @Override
+        public void onItemClick(int position, String dayText) {
+            if(dayText.equals("")){
+                String message = "Selected Date " +dayText+ " "+ monthYearFromDate(selectedDate);
+                Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
-}
