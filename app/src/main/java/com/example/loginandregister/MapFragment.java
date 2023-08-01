ab7e91f90b6741f0a00690d9441be1bc11462ad4
Earl <earlpaquibut@gmail.com>
2023-08-01 15:42:19 +0800

    Map with no API

--- app/src/main/java/com/example/loginandregister/MapFragment.java
+++ app/src/main/java/com/example/loginandregister/MapFragment.java
@@ -3,17 +3,27 @@ package com.example.loginandregister;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.fragment.app.FragmentActivity;
+import android.widget.FrameLayout;
 
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment{
+public class MapFragment extends Fragment implements OnMapReadyCallback{
+
+    private GoogleMap gMap;
+    private FrameLayout map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,8 +31,19 @@ public class MapFragment extends Fragment{
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
+        //map = view.findViewById(R.id.map);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         return view;
     }
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.gMap = googleMap;
+
+        LatLng mapLooc = new LatLng(10.3023,123.9469);
+        this.gMap.addMarker(new MarkerOptions().position(mapLooc).title("Looc"));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapLooc));
+    }
 }
