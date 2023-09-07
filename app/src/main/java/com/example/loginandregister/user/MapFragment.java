9a03f8cb87a68b4d428b735fa775fa7fc3f20b88
Earl <earlpaquibut@gmail.com>
2023-09-07 19:58:01 +0800

    Map is good but still need to add some fixed location and no zoom and adjusting view to be exact

--- app/src/main/java/com/example/loginandregister/user/MapFragment.java
+++ app/src/main/java/com/example/loginandregister/user/MapFragment.java
@@ -1,74 +1,85 @@
-package com.example.loginandregister.user;
+        package com.example.loginandregister.user;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ImageView;
+        import android.widget.ProgressBar;
 
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.Fragment;
 
-import com.example.loginandregister.R;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+        import com.example.loginandregister.R;
+        import com.google.android.gms.maps.CameraUpdateFactory;
+        import com.google.android.gms.maps.GoogleMap;
+        import com.google.android.gms.maps.SupportMapFragment;
+        import com.google.android.gms.maps.OnMapReadyCallback;
+        import com.google.android.gms.maps.model.LatLng;
+        import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback{
+        public class MapFragment extends Fragment{
 
-    private GoogleMap gMap;
-    private FrameLayout map;
-    private ProgressBar progressBar;
-    private ImageView mapPlaceholder;
+            private ProgressBar progressBar;
+            private ImageView mapPlaceholder;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
+            @Override
+            public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                     Bundle savedInstanceState) {
+                // Inflate the layout for this fragment
+                View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        progressBar = view.findViewById(R.id.progressBar);
-        mapPlaceholder = view.findViewById(R.id.mapPlaceholder);
+                progressBar = view.findViewById(R.id.progressBar);
+                mapPlaceholder = view.findViewById(R.id.mapPlaceholder);
 
-        map = view.findViewById(R.id.map);
+                SupportMapFragment supportMapFragment=(SupportMapFragment)
+                        getChildFragmentManager().findFragmentById(R.id.map);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
+                supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        double latitude, longitude;
+                        String barangay;
+
+
+                        barangay = "Looc";
+                        latitude = 10.3023;
+                        longitude = 123.9469;
+
+                        LatLng brgyMap = new LatLng(latitude, longitude);
+
+                        googleMap.addMarker(new MarkerOptions().position(brgyMap).title(barangay));
+
+                        float zoomLevel = 15;
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(brgyMap, zoomLevel));
+
+                        onMapLoaded();
+                        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                            @Override
+                            public void onMapClick(LatLng latLng) {
+                                // When clicked on map
+                                // Initialize marker options
+                                MarkerOptions markerOptions=new MarkerOptions();
+                                // Set position of marker
+                                markerOptions.position(latLng);
+                                // Set title of marker
+                                markerOptions.title(latLng.latitude+" : "+latLng.longitude);
+                                // Remove all marker
+                                googleMap.clear();
+                                // Animating to zoom the marker
+                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                                // Add marker on map
+                                googleMap.addMarker(markerOptions);
+                            }
+                        });
+                    }
+                });
+
+                return view;
+            }
+
+            public void onMapLoaded() {
+                progressBar.setVisibility(View.GONE);
+                mapPlaceholder.setVisibility(View.GONE);
+            }
         }
-
-        return view;
-    }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        double latitude, longitude;
-        String barangay;
-
-        barangay = "Looc";
-        latitude = 10.3023;
-        longitude = 123.9469;
-
-            this.gMap = googleMap;
-            LatLng brgyMap = new LatLng(latitude,longitude);
-            this.gMap.addMarker(new MarkerOptions().position(brgyMap).title(barangay));
-            this.gMap.moveCamera(CameraUpdateFactory.newLatLng(brgyMap));
-
-        onMapLoaded();
-    }
-
-    public void onMapLoaded() {
-        progressBar.setVisibility(View.GONE);
-        mapPlaceholder.setVisibility(View.GONE);
-    }
-}
--- app/src/main/java/com/example/loginandregister/user/UserMainActivity.java
+++ app/src/main/java/com/example/loginandregister/user/UserMainActivity.java
@@ -66,23 +66,6 @@ public class UserMainActivity extends AppCompatActivity {
 
     }
 
-    //Hide the Navigation Bar Method
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if(hasFocus){
-            decorView.setSystemUiVisibility(hideSystemBars());
-        }
-    }
-    private int hideSystemBars(){
-        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-    }
-
     private void replaceUserFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -92,5 +75,22 @@ public class UserMainActivity extends AppCompatActivity {
 
     public void setBottomNavigationSelectedItem(int itemId) {
         binding.bottomNavigationView.setSelectedItemId(itemId);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            decorView.setSystemUiVisibility(hideSystemBars());
+        }
+    }
+
+    private int hideSystemBars(){
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
     }
 }
