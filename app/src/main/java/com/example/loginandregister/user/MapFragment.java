024f5447608e0899e11914c32b326eb3148f9334
Aijem <aijemalmacin2018@gmail.com>
2023-09-09 13:09:28 +0800

    saving latitude and longitude to the database

--- app/src/main/java/com/example/loginandregister/user/MapFragment.java
+++ app/src/main/java/com/example/loginandregister/user/MapFragment.java
@@ -1,13 +1,17 @@
         package com.example.loginandregister.user;
 
+        import android.content.Context;
+        import android.content.SharedPreferences;
         import android.os.Bundle;
 
+        import android.provider.ContactsContract;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
         import android.widget.ImageView;
         import android.widget.ProgressBar;
 
+        import androidx.annotation.NonNull;
         import androidx.fragment.app.Fragment;
 
         import com.example.loginandregister.R;
@@ -17,11 +21,20 @@
         import com.google.android.gms.maps.OnMapReadyCallback;
         import com.google.android.gms.maps.model.LatLng;
         import com.google.android.gms.maps.model.MarkerOptions;
+        import com.google.firebase.database.DataSnapshot;
+        import com.google.firebase.database.DatabaseError;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.database.ValueEventListener;
+        import com.google.firebase.ktx.Firebase;
+
 
         public class MapFragment extends Fragment{
 
             private ProgressBar progressBar;
             private ImageView mapPlaceholder;
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference reference;
 
             @Override
             public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,6 +57,10 @@
                         barangay = "Looc";
                         latitude = 10.305712;
                         longitude = 123.941780;
+                        SharedPreferences preferences2 = getActivity().getSharedPreferences("ProfileFragment", Context.MODE_PRIVATE);
+                        String username = preferences2.getString("ProfileUsername","");
+
+
 
                         LatLng brgyMap = new LatLng(latitude, longitude);
 
@@ -55,6 +72,23 @@
                         googleMap.getUiSettings().setZoomControlsEnabled(false);
                         googleMap.getUiSettings().setZoomGesturesEnabled(false);
                         googleMap.getUiSettings().setAllGesturesEnabled(false);
+
+                        reference = database.getReference("Database").child("users").child(username);
+                        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                String bar = snapshot.child("barName").getValue(String.class);
+                                if(bar == "Looc"){
+                                    saveLocationToFirebaseLooc(latitude, longitude);
+
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
 
                         onMapLoaded();
                         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -84,5 +118,16 @@
             public void onMapLoaded() {
                 progressBar.setVisibility(View.GONE);
                 mapPlaceholder.setVisibility(View.GONE);
+            }
+            private void saveLocationToFirebaseLooc(double latitude, double longitude) {
+                // Initialize Firebase Database reference
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+                // Specify the path to the "Looc" barangay in your database
+                DatabaseReference loocReference = databaseReference.child("Database").child("Barangay").child("Looc");
+
+                // Update the latitude and longitude
+                loocReference.child("Map").child("Latitude").setValue(latitude);
+                loocReference.child("Map").child("Longitude").setValue(longitude);
             }
         }
