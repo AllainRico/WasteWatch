2d065d551b23b545e785b43fb3a5e974bb647267
Allain <allainrico@gmail.com>
2023-10-10 10:56:22 +0800

    created floating action bar for user

--- app/src/main/java/com/example/loginandregister/user/MapFragment.java
+++ app/src/main/java/com/example/loginandregister/user/MapFragment.java
@@ -14,6 +14,7 @@ import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.loginandregister.Login;
 import com.example.loginandregister.R;
 import com.example.loginandregister.admin.LocationData;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -24,12 +25,14 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class MapFragment extends Fragment {
 
@@ -48,13 +51,24 @@ public class MapFragment extends Fragment {
     private Handler handler = new Handler();
     private static final int INTERVAL = 2000;
 
+    FloatingActionButton requestCollectionBTN;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
         initWidgets(view);
+
+        requestCollectionBTN = view.findViewById(R.id.requestCollectionbtn);
+        requestCollectionBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "CLICKED~!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         SupportMapFragment supportMapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.userMap);
@@ -151,6 +165,8 @@ public class MapFragment extends Fragment {
                 });
             }
         });
+
+
         return view;
     }
 
