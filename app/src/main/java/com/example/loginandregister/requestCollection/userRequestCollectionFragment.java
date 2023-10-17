c5d8ea8d7d277091e0cf5023e94bffd15a26cad4
Muaña, Allain Rico <allainrico@gmail.com>
2023-10-17 20:16:38 +0800

    can now get the user's location. To do: send user data to firebase

--- app/src/main/java/com/example/loginandregister/requestCollection/userRequestCollectionFragment.java
+++ app/src/main/java/com/example/loginandregister/requestCollection/userRequestCollectionFragment.java
@@ -1,18 +1,29 @@
 package com.example.loginandregister.requestCollection;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.loginandregister.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,21 +35,74 @@ public class userRequestCollectionFragment extends Fragment {
     private EditText user_username_editText;
     private EditText user_barangayName;
     private EditText user_userRequestMessage_editText;
+    private Button user_checklocationButton;
+    private FusedLocationProviderClient fusedLocationProviderClient;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference reference;
-
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 200;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.user_fragment_user_request_collection, container, false);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        user_checklocationButton = view.findViewById(R.id.user_checklocationButton);
 
         displayDataToScreen(view);
-
+        getRealtimeLocation();
 
 
         return view;
+    }
+
+    private void getRealtimeLocation() {
+        if (user_checklocationButton != null) {
+            user_checklocationButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Toast.makeText(getActivity(), "Button is clicked~!", Toast.LENGTH_SHORT).show();
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                    {
+                        if(requireContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                        {
+
+                            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                                @Override
+                                public void onSuccess(Location location) {
+                                    if(location != null)
+                                    {
+
+                                        Double user_lat_value = location.getLatitude();
+                                        Double user_long_value = location.getLongitude();
+                                        Toast.makeText(getActivity(), "Latitude = "+ user_lat_value + " Longitude = " + user_long_value, Toast.LENGTH_SHORT).show();
+
+
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getActivity(), "!!!!!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                        else
+                        {
+                            Toast.makeText(getActivity(), "!!!", Toast.LENGTH_SHORT).show();
+                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                        }
+                    }
+                    else
+                    {
+                        Toast.makeText(getActivity(), "ELSE Button is clicked~!", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        } else {
+            Log.e("ButtonError", "user_check_location_button is null");
+        }
     }
 
     private void displayDataToScreen(View view) {
