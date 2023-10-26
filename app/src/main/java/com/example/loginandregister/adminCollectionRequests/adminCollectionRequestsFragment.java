8fb84ac52df84391c2446844f5a912aa602db2e9
Muaña, Allain Rico <allainrico@gmail.com>
2023-10-26 22:02:51 +0800

    onlick = move to mapfragment

--- app/src/main/java/com/example/loginandregister/adminCollectionRequests/adminCollectionRequestsFragment.java
+++ app/src/main/java/com/example/loginandregister/adminCollectionRequests/adminCollectionRequestsFragment.java
@@ -4,19 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.loginandregister.R;
+import com.example.loginandregister.admin.AdminMapFragment;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 
 import java.util.ArrayList;
 
@@ -72,6 +71,14 @@ public class adminCollectionRequestsFragment extends Fragment implements UserDat
     @Override
     public void onItemClick(int position) {
         UserDataModel clickedItem = userDataList.get(position);
-        Toast.makeText(getContext(), "Clicked: " + clickedItem.getUsername(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getContext(), "Clicked: " + clickedItem.getUsername(), Toast.LENGTH_SHORT).show();
+
+        AdminMapFragment adminMapFragment = new AdminMapFragment();
+        getActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, adminMapFragment)
+                .addToBackStack(null)
+                .commit();
+
     }//onItemClick
 }//adminCollectionRequestsFragment
